c4679a8289067aab5e56221ac69ebd2911a8388c
朱正晶 <zhuzhengjing@zhuzhengjingdeMac.local>
2015-02-27 15:15:41 +0800

    Initial Commit

---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by 朱正晶 on 15-2-27.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/BNRViewController.m
@@ -0,0 +1,29 @@
+//
+//  BNRViewController.m
+//  Hypnosister
+//
+//  Created by 朱正晶 on 15-2-27.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import "BNRViewController.h"
+
+@interface BNRViewController ()
+
+@end
+
+@implementation BNRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
